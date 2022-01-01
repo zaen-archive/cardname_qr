2721f3bc861051f8fe2b1f4f32ed0925627ca287
zaen <zylancer@gmail..com>
2022-01-02 06:08:05 +0700

    finish carname qrcode

--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module generate_qr
 
 go 1.17
+
+require (
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/jung-kurt/gofpdf v1.16.2 // indirect
+	github.com/lithammer/shortuuid/v3 v3.0.7 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/phpdave11/gofpdi v1.0.13 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/signintech/gopdf v0.10.6 // indirect
+	github.com/skip2/go-qrcode v0.0.0-20200617195104-da1b6568686e // indirect
+)
--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"image"
 	"image/draw"
 	"image/png"
 	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+	"github.com/lithammer/shortuuid/v3"
+	"github.com/signintech/gopdf"
+	"github.com/skip2/go-qrcode"
 )
 
-func getCard(filepath string) *image.RGBA {
+func createCardName(filepath string) *image.RGBA {
 	oriFile, err := os.Open(filepath)
 	if err != nil {
 		panic(err)
@@ -28,13 +35,74 @@ func getCard(filepath string) *image.RGBA {
 	return modPng
 }
 
+func addQrCode(card *image.RGBA) {
+	id := shortuuid.New()
+	url := os.Getenv("QR_PREFIX") + id
+
+	x, _ := strconv.Atoi(os.Getenv("QR_X"))
+	y, _ := strconv.Atoi(os.Getenv("QR_Y"))
+	size, _ := strconv.Atoi(os.Getenv("QR_SIZE"))
+
+	qrSource, err := qrcode.Encode(url, qrcode.Highest, size)
+	if err != nil {
+		panic(err)
+	}
+
+	qrImg, err := png.Decode(bytes.NewReader(qrSource))
+	if err != nil {
+		panic(err)
+	}
+
+	for i := 0; i < size; i++ {
+		for j := 0; j < size; j++ {
+			color := qrImg.At(i, j)
+			card.Set(x+i, y+j, color)
+		}
+	}
+}
+
 func main() {
-	card := getCard("assets/card.png")
-	file, _ := os.Create("temp/card2.png")
+	err := godotenv.Load()
+	if err != nil {
+		panic(err)
+	}
 
-	defer file.Close()
+	for i := 0; i < 10; i++ {
+		card := createCardName(os.Getenv("CARD_PATH"))
 
-	png.Encode(file, card)
+		addQrCode(card)
 
+		file, _ := os.Create("temp/card/card-" + strconv.Itoa(i) + ".png")
+
+		png.Encode(file, card)
+		file.Close()
+	}
+
+	pdf := gopdf.GoPdf{}
+	pdf.Start(gopdf.Config{PageSize: *gopdf.PageSizeA4})
+	pdf.AddPage()
+
+	cardWidth, _ := strconv.ParseFloat(os.Getenv("CARD_WIDTH"), 64)
+	cardHeight, _ := strconv.ParseFloat(os.Getenv("CARD_HEIGHT"), 64)
+	// cardFlipped := os.Getenv("CARD_FLIP") == "true"
+
+	cardWidth = gopdf.UnitsToPoints(gopdf.UnitMM, cardWidth)
+	cardHeight = gopdf.UnitsToPoints(gopdf.UnitMM, cardHeight)
+	pdfRect := &gopdf.Rect{
+		W: cardWidth,
+		H: cardHeight,
+	}
+
+	for i := 0; i < 2; i++ {
+		for j := 0; j < 5; j++ {
+			x := float64(i) * cardWidth
+			y := float64(j) * cardHeight
+			index := i*5 + j
+
+			pdf.Image("temp/card/card-"+strconv.Itoa(index)+".png", x, y, pdfRect)
+		}
+	}
+
+	pdf.WritePdf("temp/hello.pdf")
 	fmt.Println("~~ Finish ~~")
 }
